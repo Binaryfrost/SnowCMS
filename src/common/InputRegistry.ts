b76578cdc73f74166132bfde74b0ab3e1bc22bf6
TrueWinter <nicholis@truewinter.dev>
2024-12-13 18:29:11 +0200

    Updated doc comments in InputRegistry

--- src/common/InputRegistry.ts
+++ src/common/InputRegistry.ts
@@ -6,58 +6,110 @@ import type { Collection } from './types/Collection';
 
 export interface InputRef<T> {
   // It's possible for the Input to not return a value (e.g. notices/alerts)
+  /**
+   * Return the value of this Input
+   */
   getValues?: () => T
+  /**
+   * Returns whether there are any errors in this Input.
+   * This only prevents the Collection Entry form from
+   * being submitted, it does not show the error message.
+   * You are required to write code to render the error.
+   */
   hasError?: () => boolean | Promise<boolean>
+
+  /**
+   * Called when other Inputs are updated. Keep in mind that
+   * not all Inputs notify other Inputs of changes.
+   */
   notifyFormUpdate?: (values: Record<string, any>) => void
 }
 
 export interface InputProps<T, S> {
+  /**
+   * The Input's value
+   */
   value: T
+  /**
+   * The Input's settings
+   */
   settings: S
+  /**
+   * The name set in the Collection settings
+   */
   name: string
+  /**
+   * The field name (used in API responses) set in the Collection settings
+   */
   fieldName: string
+  /**
+   * The description set in the Collection settings
+   */
   description?: string
+  /**
+   * Called to notify other Inputs of changes to this one
+   */
   notifyChanges: () => void
 }
 
 type RenderHtmlType<T> = string | T | Object;
 
 interface BaseInput<T, S> {
-  // Provided ID (in this example, "text")
+  /**
+   * Input ID, must be unique and should include
+   * your plugin name for namespacing purposes.
+   * Shown in the Collection settings.
+   *
+   * **You should not change this after using the
+   * Input as doing so will result in null being
+   * returned anywhere where this Input is used.**
+   */
   id: string
 
-  // Name and description shown for this input in the Collection settings
+  /**
+   * Name shown for this input in the Collection settings
+   */
   name: string
+
+  /**
+   * Description shown for this input in the Collection settings
+   */
   description?: string
 
-  /*
-   * Serialization methods.
-   * serialize is called before sending data to server (client-side only)
-   * deserialize is called before rendering input and HTML (client and server side)
+  /**
+   * Called before sending data to server (client-side only)
    */
   serialize: (data: T) => string
+  /**
+   * Called before rendering input and HTML (client and server side)
+   */
   deserialize: (data: string) => T
 
-  // Called client-side to render input in CMS; remember to call useImperativeHandle with InputRef object if the Input has a value
-  /*
+  /**
+   * Called client-side to render input in CMS; remember to call useImperativeHandle
+   * with InputRef object if the Input has a value
+   *
    * Example usage:
+   * ```js
    * const InputComponent = input.renderInput();
    * <InputComponent value={value} settings={settings} label={name} description={description} />
+   * ```
    */
   renderInput: () =>
     ForwardRefExoticComponent<InputProps<T, S> & RefAttributes<InputRef<T>>> |
     FunctionComponent<InputProps<T, S>> | undefined
 
-  /*
+  /**
    * Called client-side to determine whether to show in input library
    * and server-side to validate that the request does not contain input.
    * Defaults to true
    */
   isAllowed?: (website: Website, collection: Collection) => boolean
 
-  /*
+  /**
    * Called server-side when page is requested through API
    * with the query parameter `?render=true`.
+   *
    * If you don't want the input to be rendered as HTML,
    * return a JSON object. You can also return a non-HTML string.
    */
@@ -70,10 +122,20 @@ interface SettingsProps<S> {
 }
 
 interface InputWithSettings<T, S> extends BaseInput<T, S> {
-  // Called client-side to render settings in Collection settings
+  /**
+   * Called client-side to render settings in Collection settings
+   */
   renderSettings: () =>
     ForwardRefExoticComponent<SettingsProps<S> & RefAttributes<InputRef<S>>>
+
+  /**
+   * Serializes settings to a string
+   */
   serializeSettings: (data: S) => string
+
+  /**
+   * Deserializes settings from a string
+   */
   deserializeSettings: (data: string) => S
 }
 
