587e004c0eb3ffc5f728f7478f2340175f92b72b
TrueWinter <nicholis@truewinter.dev>
2025-01-22 18:49:58 +0200

    Updated client `getCollectionEntriesData` type

--- src/lib/client/index.d.ts
+++ src/lib/client/index.d.ts
@@ -1,5 +1,5 @@
 import type { PaginatedQueryClient, PaginatedQueryOpts } from './PaginatedQueryClient';
-import type { CollectionEntry, CollectionEntryWithData } from './types/CollectionEntry';
+import type { CollectionEntry, CollectionEntryWithRenderedData } from './types/CollectionEntry';
 
 export default class SnowCMSClient {
   constructor(url: string, websiteId: string, apiKey: string);
@@ -10,6 +10,6 @@ export default class SnowCMSClient {
   getCollectionEntries<T extends CollectionEntry = CollectionEntry>
     (collection: string, opts?: PaginatedQueryOpts): PaginatedQueryClient<T>;
 
-  getCollectionEntriesData<T extends CollectionEntryWithData = CollectionEntryWithData>
-    (entries: CollectionEntry[]): Promise<T[]>;
+  getCollectionEntriesData<T = Record<string, any>>
+    (entries: CollectionEntry[]): Promise<CollectionEntryWithRenderedData<T>>;
 }
