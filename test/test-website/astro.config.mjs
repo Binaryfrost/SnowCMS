042dd986d263c5400977811b08e433b009c9d206
TrueWinter <nicholis@truewinter.dev>
2026-04-07 22:54:40 +0200

    Test website: Added option to render HTML

--- test/test-website/astro.config.mjs
+++ test/test-website/astro.config.mjs
@@ -9,7 +9,8 @@ export default defineConfig({
       CMS_API: envField.string({ context: 'server', access: 'secret' }),
       CMS_API_KEY: envField.string({ context: 'server', access: 'secret' }),
       CMS_WEBSITE: envField.string({ context: 'server', access: 'secret' }),
-      CMS_COLLECTION: envField.string({ context: 'server', access: 'secret' })
+      CMS_COLLECTION: envField.string({ context: 'server', access: 'secret' }),
+      CMS_RENDER_HTML: envField.boolean({ context: 'server', access: 'secret' })
     }
   },
   devToolbar: {
